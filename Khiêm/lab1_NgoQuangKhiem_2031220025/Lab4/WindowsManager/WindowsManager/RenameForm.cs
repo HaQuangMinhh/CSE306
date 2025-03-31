3b9bd421a00e029c45b36b48d7547cb9c6cd398e
HaQuangMinhh <155843525+HaQuangMinhh@users.noreply.github.com>
2025-03-31 16:39:49 +0700

    Add files via upload

---
+++ Khiêm/lab1_NgoQuangKhiem_2031220025/Lab4/WindowsManager/WindowsManager/RenameForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsManager
+{
+    public partial class RenameForm : Form
+    {
+        public RenameForm()
+        {
+            InitializeComponent();
+        }
+        public RenameForm(string _s)
+        {
+            InitializeComponent();
+            txtNewName.Text = _s;
+        }
+        public string ReturnValue { get; private set; }
+
+        private void btnChangeName_Click(object sender, EventArgs e)
+        {
+            ReturnValue = txtNewName.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
